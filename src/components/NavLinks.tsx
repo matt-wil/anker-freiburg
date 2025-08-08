31ce088f0ffd966a58984ef0b37f5630fc562f6f
Matt Williams <matthew.steven.williams@gmail.com>
2025-08-08 20:20:32 +0200

    refactor(aktionen.tsx): typesafe the file

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { useState, useRef } from 'react';
-import { CiMenuFries} from 'react-icons/ci';
-import { IoIosClose } from 'react-icons/io';
-import { useTranslations } from 'next-intl';
-import NavigationLink from './NavigationLink';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
+import { useState, useRef } from "react";
+import { CiMenuFries } from "react-icons/ci";
+import { IoIosClose } from "react-icons/io";
+import { useTranslations } from "next-intl";
+import NavigationLink from "./NavigationLink";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 interface NavItem {
   key: string;
@@ -15,13 +15,13 @@ interface NavItem {
 }
 
 const navLinks: NavItem[] = [
-  { key: 'home', path: '', label: 'nav.home' },
-  { key: 'about', path: 'ueber-uns', label: 'nav.about' },
-  { key: 'tattoo', path: 'tattoo', label: 'nav.tattoo' },
-  { key: 'piercing', path: 'piercing', label: 'nav.piercing' },
-  { key: 'kontakt', path: 'kontakt', label: 'nav.contact' },
-  { key: 'aktionen', path: 'aktionen', label: 'nav.promo' },
-  { key: 'faq', path: 'haeufige-gestellte-fragen', label: 'nav.faq' },
+  { key: "home", path: "", label: "nav.home" },
+  { key: "about", path: "ueber-uns", label: "nav.about" },
+  { key: "tattoo", path: "tattoo", label: "nav.tattoo" },
+  { key: "piercing", path: "piercing", label: "nav.piercing" },
+  { key: "kontakt", path: "kontakt", label: "nav.contact" },
+  { key: "aktionen", path: "aktionen", label: "nav.promo" },
+  { key: "faq", path: "haeufige-gestellte-fragen", label: "nav.faq" },
 ];
 
 const NavLinks = () => {
@@ -36,12 +36,12 @@ const NavLinks = () => {
   useGSAP(
     () => {
       if (isOpen && menuRef.current) {
-        gsap.set(menuRef.current, { y: '-100%' });
+        gsap.set(menuRef.current, { y: "-100%" });
 
         gsap.to(menuRef.current, {
           y: 0,
           duration: 1,
-          ease: 'power2.out',
+          ease: "power2.out",
         });
 
         gsap.from(linksRef.current, {
@@ -50,20 +50,20 @@ const NavLinks = () => {
           stagger: 0.1,
           delay: 0.2,
           duration: 0.75,
-          ease: 'power3.out',
+          ease: "power3.out",
         });
       } else if (!isOpen && menuRef.current) {
         gsap.to(menuRef.current, {
-          y: '100%',
+          y: "100%",
           duration: 0.5,
-          ease: 'power3.in',
+          ease: "power3.in",
           onComplete: () => {
-            setShouldRender(false); 
+            setShouldRender(false);
           },
         });
       }
     },
-    { dependencies: [isOpen] }
+    { dependencies: [isOpen] },
   );
 
   const handleOpen = () => {
@@ -78,12 +78,12 @@ const NavLinks = () => {
   return (
     <nav className="relative z-50">
       {isOpen ? (
-            <IoIosClose
-              className="w-20 h-20 cursor-pointer text-black fixed top-4 right-4 z-60"
-              onClick={handleClose}
-              role={`button`}
-              aria-label={`Close Menu`}
-            />
+        <IoIosClose
+          className="w-20 h-20 cursor-pointer text-black fixed top-4 right-4 z-60"
+          onClick={handleClose}
+          role={`button`}
+          aria-label={`Close Menu`}
+        />
       ) : (
         <CiMenuFries
           className="w-10 h-10 cursor-pointer"
@@ -110,28 +110,28 @@ const NavLinks = () => {
               </NavigationLink>
             </div>
           ))}
-          <div className='flex flex-row justify-between p-2 text-xs uppercase'>
-            <div className='flex flex-col'>
-            <a
+          <div className="flex flex-row justify-between p-2 text-xs uppercase">
+            <div className="flex flex-col">
+              <a
                 className="hover:animate-pulse hover:text-white"
                 href="https://www.instagram.com/anker.tattoo.freiburg?igsh=MTdtOxIkYXIxMHE0bw=="
                 target="_blank"
                 rel="noopener noreferrer"
-        >
+              >
                 <span>instagram &#8599;</span>
-            </a>
-            <a
-                className='hover:animate-pulse hover:text-white'
-                href='https://www.facebook.com/profile.php?id=61579347080829'
-                target='_blank'
-                rel='noopener noreferrer'
-                >
+              </a>
+              <a
+                className="hover:animate-pulse hover:text-white"
+                href="https://www.facebook.com/profile.php?id=61579347080829"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <span>facebook &#8599;</span>
-            </a>
+              </a>
             </div>
-            <div className='flex flex-col mr-4'>
-                <a href="mailto:info@anker-tattoo.de">info@anker-tattoo.de</a>
-                <a href="tel:+4976151462878">076151462878</a>
+            <div className="flex flex-col mr-4">
+              <a href="mailto:info@anker-tattoo.de">info@anker-tattoo.de</a>
+              <a href="tel:+4976151462878">076151462878</a>
             </div>
           </div>
         </div>
