30f2c9c3b03e829659fe3d1d10e9d455a9a51e1a
Matt Williams <matthew.steven.williams@gmail.com>
2025-08-08 20:36:01 +0200

    fix inter error in layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,45 +1,42 @@
-import '@/globals.css'
-import { Inter } from 'next/font/google'
-import type { Metadata } from 'next'
-import Navbar from '@/components/Navbar'
-import Footer from "@/components/Footer"
-import { NextIntlClientProvider } from 'next-intl'
-import {notFound} from 'next/navigation';
-import {Locale, routing} from '@/i18n/routing';
-import IntroWrapper from '@/components/IntroWrapper'
+import "@/globals.css";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
+import { Locale, routing } from "@/i18n/routing";
+import IntroWrapper from "@/components/IntroWrapper";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: 'Anker - Tattoo und Piercing Studio in Freiburg',
-    template: '%s | Anker - Tattoo und Piercing Studio in Freiburg',
-  
+    default: "Anker - Tattoo und Piercing Studio in Freiburg",
+    template: "%s | Anker - Tattoo und Piercing Studio in Freiburg",
   },
-  description: 'Dein neues Tattoo oder Piercing in Freiburg? Bei uns bist du goldrichtig! Professionelle Beratung - Top-Künstler - Kurze Wartezeiten',
-}
+  description:
+    "Dein neues Tattoo oder Piercing in Freiburg? Bei uns bist du goldrichtig! Professionelle Beratung - Top-Künstler - Kurze Wartezeiten",
+};
 
 export default async function LocaleLayout({
   children,
-  params
+  params,
 }: {
-  children: React.ReactNode
-  params: Promise<{locale: string}>
+  children: React.ReactNode;
+  params: { locale: string };
 }) {
-
-  const {locale} = await params;
+  const { locale } = params;
   if (!routing.locales.includes(locale as Locale)) {
     notFound();
   }
 
-
   return (
     <html lang={locale}>
       <meta name="apple-mobile-web-app-title" content="AnkerFr" />
-      <body className={`${inter.className}`}>
+      <body className={inter.className}>
         <NextIntlClientProvider locale={locale}>
-          <IntroWrapper >
+          <IntroWrapper>
             <Navbar />
             <main>{children}</main>
             <Footer />
@@ -47,5 +44,5 @@ export default async function LocaleLayout({
         </NextIntlClientProvider>
       </body>
     </html>
-  )
+  );
 }
