ce9ef4fd9a103748ee7bed8c9211ddccbe9dbdf8
Matt Williams <matthew.steven.williams@gmail.com>
2025-08-08 22:57:26 +0200

    change video player

--- src/components/OptimizedVideo.tsx
+++ src/components/OptimizedVideo.tsx
@@ -1,40 +1,39 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
-import { CldVideoPlayer } from 'next-cloudinary';
-import 'next-cloudinary/dist/cld-video-player.css';
+import { useEffect, useState } from "react";
+import { CldVideoPlayer } from "next-cloudinary";
+import "next-cloudinary/dist/cld-video-player.css";
 
 interface OptimizedVideoProps {
-  src: string
-  className?: string
+  src: string;
+  className?: string;
 }
- 
 
-
-const OptimizedVideo = ({src, className}: 
-  OptimizedVideoProps
-): React.JSX.Element=> {
+const OptimizedVideo = ({
+  src,
+  className,
+}: OptimizedVideoProps): React.JSX.Element => {
   const [videoSrc, setVideoSrc] = useState<string | null>(null);
 
   useEffect(() => {
-    setVideoSrc(src)
-  },[src])
+    setVideoSrc(src);
+  }, [src]);
 
   if (!videoSrc) {
     return <div>Loading...</div>;
   }
 
   return (
-        <CldVideoPlayer
-            src={videoSrc}
-            autoPlay
-            loop
-            muted
-            className={className}
-            fontFace="Source Serif Pro"
-            playsinline
-        />
-  )
-}
+    <CldVideoPlayer
+      src={videoSrc}
+      autoPlay
+      f-auto
+      loop
+      muted
+      className={className}
+      fontFace="Source Serif Pro"
+    />
+  );
+};
 
-export default OptimizedVideo
+export default OptimizedVideo;
