1714d8658fa99ee144ebfb9d5a8491d58203688d
Matt Williams <matthew.steven.williams@gmail.com>
2025-08-08 20:47:24 +0200

    fix sitempa

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.NEXT_SITE_URL || 'https://www.anker-tattoo.de',
-    generateRobotsTxt: true,
-    exclude: ["/server-sitemap.xml", "/blog/*"],
-    robotsTxtOptions: {
-        policies: [
-            {
-                userAgent: '*',
-                disallow: '/admin'
-            },
-            {
-                userAgent: '*',
-                allow: '/'
-            }
-        ],
-        additionalSitemaps: [
-            `${process.env.NEXT_SITE_URL}/server-sitemap.xml`
-        ]
-    }
-  }
+  siteUrl: process.env.NEXT_SITE_URL || "https://www.anker-tattoo.de",
+  generateRobotsTxt: true,
+  exclude: ["/server-sitemap.xml", "/blog/*"],
+  changefreq: "weekly",
+  priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: "/admin",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    additionalSitemaps: [`${process.env.NEXT_SITE_URL}/server-sitemap.xml`],
+  },
+};
--- src/app/server-sitemap.xml/route.tsx
+++ src/app/server-sitemap.xml/route.tsx
@@ -1,11 +1,23 @@
 import { getServerSideSitemap, ISitemapField } from "next-sitemap";
 
-export async function GET(request: Request) {
-    const res = await fetch("https://anker-freiburg.de/api/artists", {cache: 'no-store'});
-    const json = await res.json()
-    const data: any[] = json['items']
+interface Artist {
+  slug: string;
+}
 
-    const fields: ISitemapField[] = data.map((e: any) => ({ loc: `https://anker-freiburg.de/artists/${e['slug']}`, lastmod: new Date().toISOString() }))
+const siteUrl = process.env.NEXT_SITE_URL ?? "http://localhost:3000";
 
-    return getServerSideSitemap(fields);
+export async function GET() {
+  const res = await fetch(`${siteUrl}/api/artists`, {
+    cache: "no-store",
+  });
+
+  const json: { items: Artist[] } = await res.json();
+  const data = json.items;
+
+  const fields: ISitemapField[] = data.map((artist) => ({
+    loc: `${siteUrl}/artists/${artist.slug}`,
+    lastmod: new Date().toISOString(),
+  }));
+
+  return getServerSideSitemap(fields);
 }
