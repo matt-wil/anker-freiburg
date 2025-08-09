8361dae347cd409838a709f505176171566ebda7
Matt Williams <matthew.steven.williams@gmail.com>
2025-08-09 22:33:25 +0200

    bit of eslint fixes and addint in the creator link for the header

--- src/components/Piercers.tsx
+++ src/components/Piercers.tsx
@@ -1,8 +1,8 @@
-import { getPiercingArtists } from '@/lib/queries/artists';
-import ImageCard from './ImageCard';
-import PiercingPriceList from './PiercingPriceList';
-import PiercingJewelleryGallery from './PiercingJewelleryGallery';
-import ScrollButton from './ScrollButton';
+import { getPiercingArtists } from "@/lib/queries/artists";
+import ImageCard from "./ImageCard";
+import PiercingPriceList from "./PiercingPriceList";
+import PiercingJewelleryGallery from "./PiercingJewelleryGallery";
+import ScrollButton from "./ScrollButton";
 
 export default async function Piercers() {
   const artists = await getPiercingArtists();
@@ -11,8 +11,8 @@ export default async function Piercers() {
     <section className="px-6 max-w-6xl mx-auto">
       <h1 className="page-header font-bold mb-8">Professional Body Piercers</h1>
 
-      <ScrollButton targetId='price-list'>Preiselist</ScrollButton>
-      <ScrollButton targetId='schmuck'>Schmuck</ScrollButton>
+      <ScrollButton targetId="price-list">Preisliste</ScrollButton>
+      <ScrollButton targetId="schmuck">Schmuck</ScrollButton>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {artists.map((artist) => {
@@ -21,26 +21,29 @@ export default async function Piercers() {
             return null;
           }
           return (
-          <a
-            key={artist.slug}
-            href={`/piercing/${artist.slug}`}
-            className="text-black border rounded-xl p-4 bg-white/60 hover:shadow-lg transition"
-          >
-            <div className="mb-4 overflow-hidden mx-auto">
-              <ImageCard
-                showHoverEffect={true}
-                src={`${publicId}`}
-                alt={`Anker Tattoo & Piercing Studio in Freiburg Artist ${artist.name} Profile Image`}
-                width={300}
-                height={300}
-                crop="fill"
-                gravity="face"
-                className="rounded object-cover group-hover:scale-105 transition-transform"
-              />
-            </div>
-            <h2 className="text-xl font-semibold text-center">{artist.name}</h2>
-            <p className="text-sm text-gray-600 text-center">{artist.bio}</p>
-          </a>)
+            <a
+              key={artist.slug}
+              href={`/piercing/${artist.slug}`}
+              className="text-black border rounded-xl p-4 bg-white/60 hover:shadow-lg transition"
+            >
+              <div className="mb-4 overflow-hidden mx-auto">
+                <ImageCard
+                  showHoverEffect={true}
+                  src={`${publicId}`}
+                  alt={`Anker Tattoo & Piercing Studio in Freiburg Artist ${artist.name} Profile Image`}
+                  width={300}
+                  height={300}
+                  crop="fill"
+                  gravity="face"
+                  className="rounded object-cover group-hover:scale-105 transition-transform"
+                />
+              </div>
+              <h2 className="text-xl font-semibold text-center">
+                {artist.name}
+              </h2>
+              <p className="text-sm text-gray-600 text-center">{artist.bio}</p>
+            </a>
+          );
         })}
       </div>
       <div id="price-list">
