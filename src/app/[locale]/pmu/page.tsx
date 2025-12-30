a0ee958eff5ac1955f3ae3621edfb49d16a56aed
Matt Williams <matthew.steven.williams@gmail.com>
2025-12-30 21:03:07 +0100

    feat(beauty): create base and setup permanent make up page

---
+++ src/app/[locale]/pmu/page.tsx
@@ -0,0 +1,38 @@
+import Pmu from "@/components/Pmu";
+import { getTranslations } from "next-intl/server";
+import { Metadata } from "next";
+import { createPageMetadata } from "@/lib/metadata";
+
+type RouteParams = { locale: string };
+
+type SegmentProps = { params: Promise<RouteParams> };
+
+export async function generateMetadata({
+  params,
+}: SegmentProps): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "pmu.metadata" });
+
+  const translatedPath = t("pathSlug");
+
+  return createPageMetadata({
+    title: t("title"),
+    description: t("description"),
+    keywords: t("keywords").split(", "),
+    ogTitle: t("ogTitle"),
+    ogDescription: t("ogDescription"),
+    ogImageAlt: t("ogImageAlt"),
+    path: `/${translatedPath}`,
+    locale,
+  });
+}
+
+const page = (): React.JSX.Element => {
+  return (
+    <>
+      <Pmu />
+    </>
+  );
+};
+
+export default page;
---
+++ src/components/Pmu.tsx
@@ -0,0 +1,3 @@
+export default function Pmu() {
+  return <div>Permanent makeup </div>;
+}
